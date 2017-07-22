076db8d219eec820b1bcba2fd3b1bf5a1446363c
Dal Shamsher Garbuja Pun <dsgarbujapun@gmail.com>
2017-07-22 19:04:28 +1000

    Implemented exception for non-integer number

--- NumGame.py
+++ NumGame.py
@@ -9,18 +9,23 @@ import random
 
 def game():
     # generate a random number between 1 and 10
+
     secret_num = random.randint(1, 10)
 
     while True:
-        # get a number guess from the player
-        guess = int(input("Guess a number between 1 and 10: :"))
-
-        # compare guess to secret number
-        if guess == secret_num:
-            print("You got it! My number was {}".format(secret_num))
-            break
+        guess = 0
+        try:
+            # get a number guess from the player
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("{} is not a number".format(guess))
         else:
-            print("That's not it!")
+            # compare guess to secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
+            else:
+                print("That's not it!")
 
 
 game()
