fcfa536c633df9b22b755c2aa80825279e76bb4c
Dal S. Garbuja Pun <dsgarbujapun@gmail.com>
2017-07-23 17:53:07 +1000

    TODOs lekhyo

---
+++ letter_khel.py
@@ -0,0 +1,13 @@
+# letter guess garnay game
+# yo game le list bata random word chancha, ani hamilai letter guess garna lagaucha
+# jaba samma hamilay right guess gardainau athawa  hamro chance siddincha
+#
+# TODOs:
+# sabda ko list banaunay
+# euta random word tyo list bata tipnay
+# space draw garnay
+# guess garnay
+# guessed letters ra strikes(kati wata letter miss bhayeko bhanayra) draw garnay
+# jityo ki haryo bhanayra output garnay
+
+
